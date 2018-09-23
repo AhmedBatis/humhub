a689d43656fe5867444cbad1f8805bb3c863afec
Ahmed Batis <mr.batis@gmail.com>
2018-09-23 10:55:52 +0400

    Update views_activities_ActivitySpaceCreated.php

--- protected/humhub/modules/activity/messages/ar/views_activities_ActivitySpaceCreated.php
+++ protected/humhub/modules/activity/messages/ar/views_activities_ActivitySpaceCreated.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  '%displayName% created the new space %spaceName%' => '%displayName% قام بافتتاح باحة جديدة %spaceName%',
-  '%displayName% created this space.' => '%displayName% قام بافتتاح هذه الباحة',
+'%displayName% created the new space %spaceName%' => 'قام المستخدم %displayName% بإنشاء ساحة جديدة باسم %spaceName%',
+'%displayName% created this space.' => '%displayName% قام بإنشاء هذه الساحة.',
 );

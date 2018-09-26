0cad9049eb64a45523caa0514ccc238d6ec7095c
Ahmed Batis <mr.batis@gmail.com>
2018-09-26 15:39:32 +0400

    Update views_setting_oembed_edit.php

--- protected/humhub/modules/admin/messages/ar/views_setting_oembed_edit.php
+++ protected/humhub/modules/admin/messages/ar/views_setting_oembed_edit.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'Add OEmbed provider' => '',
-  'Back to overview' => '',
-  'Delete' => 'حذف',
-  'Edit OEmbed provider' => '',
-  'Save' => 'حفظ',
-  'Url Prefix without http:// or https:// (e.g. youtube.com)' => '',
-  'Use %url% as placeholder for URL. Format needs to be JSON. (e.g. http://www.youtube.com/oembed?url=%url%&format=json)' => '',
+    'Add OEmbed provider' => 'إضافة مزود OEmbed',
+    'Back to overview' => 'العودة للقائمة العامة',
+    'Delete' => 'حذف',
+    'Edit OEmbed provider' => 'تعديل مزود OEmbed',
+    'Save' => 'حفظ',
+    'Url Prefix without http:// or https:// (e.g. youtube.com)' => 'بدون سابقة بروتوكول http أو https، مباشرة هكذا youtube.com مثلا',
+    'Use %url% as placeholder for URL. Format needs to be JSON. (e.g. http://www.youtube.com/oembed?url=%url%&format=json)' => '',
 );

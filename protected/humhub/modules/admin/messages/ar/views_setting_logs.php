fc06a1fcf0732ac39dd56687e76a4db1530bb277
Ahmed Batis <mr.batis@gmail.com>
2018-09-26 15:51:53 +0400

    Update views_setting_logs.php

--- protected/humhub/modules/admin/messages/ar/views_setting_logs.php
+++ protected/humhub/modules/admin/messages/ar/views_setting_logs.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'Currently there are {count} records in the database dating from {dating}.' => '',
-  'Old logs can significantly increase the size of your database while providing little information.' => '',
-  'Save' => 'حفظ',
+    'Currently there are {count} records in the database dating from {dating}.' => 'هناك سجلات بعدد {count} في قاعدة بياناتك منذ الفترة {dating}.',
+    'Old logs can significantly increase the size of your database while providing little information.' => 'السجلات القديمة بإمكانها زيادة حجم قاعدة البيانات بشكل كبير، وفي الوقت نفسه لا تعطي معلومات مفصلة ومهمة غالباً.',
+    'Save' => 'حفظ',
 );

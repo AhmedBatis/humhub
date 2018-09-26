b25cce3bca23375774372279dfa10cd923cdf645
Ahmed Batis <mr.batis@gmail.com>
2018-09-26 14:47:32 +0400

    Update views_user_delete.php

--- protected/humhub/modules/admin/messages/ar/views_user_delete.php
+++ protected/humhub/modules/admin/messages/ar/views_user_delete.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  'All created contents of this user will be <b>deleted</b>.' => '',
-  'Are you sure you want to delete this user?' => '',
-  'Cancel' => 'إلغاء',
-  'Confirm user deletion' => '',
-  'Delete user' => '',
-  'If this user is owner of some spaces, <b>you</b> will automatically become owner of these spaces.' => '',
+    'All created contents of this user will be <b>deleted</b>.' => 'سيتم <b>حذف</b> كافة المحتويات التي قام بإنشاءها هذا العضو.',
+    'Are you sure you want to delete this user?' => 'هل أنت متأكد من حذف هذا المستخدم؟',
+    'Cancel' => 'إلغاء',
+    'Confirm user deletion' => 'تأكيد حذف المستخدم',
+    'Delete user' => 'حذف المستخدم',
+    'If this user is owner of some spaces, <b>you</b> will automatically become owner of these spaces.' => 'ستكون <b>أنت</b> المالك لكافة الساحات التي قام بإنشاءها هذا العضو، حيث ستنتقل ملكيتها تلقائيا إليك.',
 );

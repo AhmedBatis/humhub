c6c24579a75fad15bdc16d7390eeecb1a59940a9
Ahmed Batis <mr.batis@gmail.com>
2018-09-29 17:55:08 +0400

    Update views_group_manageGroupUser.php

--- protected/humhub/modules/admin/messages/ar/views_group_manageGroupUser.php
+++ protected/humhub/modules/admin/messages/ar/views_group_manageGroupUser.php
@@ -17,8 +17,8 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Add new members...' => '',
-    'No' => '',
-    'Remove from group' => '',
-    'Yes' => '',
+    'Add new members...' => 'إضافة مستخدمين جدد...',
+    'No' => 'لا',
+    'Remove from group' => 'إزالة من المجموعة',
+    'Yes' => 'نعم',
 ];

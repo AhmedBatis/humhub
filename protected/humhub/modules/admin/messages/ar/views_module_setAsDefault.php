6f4acd11ea5bcdc3ed36194bef01cc6e7cafd07b
Ahmed Batis <mr.batis@gmail.com>
2018-09-29 15:22:27 +0400

    Update views_module_setAsDefault.php

--- protected/humhub/modules/admin/messages/ar/views_module_setAsDefault.php
+++ protected/humhub/modules/admin/messages/ar/views_module_setAsDefault.php
@@ -1,12 +1,12 @@
 <?php
 return array (
-  '%moduleName% - Set as default module' => '',
-  'Activated' => 'تم التفعيل',
-  'Always activated' => '',
-  'Close' => 'اغلاق',
-  'Deactivated' => '',
-  'Here you can choose whether or not a module should be automatically activated on a space or user profile. If the module should be activated, choose "always activated".' => '',
-  'Save' => 'حفظ',
-  'Spaces' => 'الباحات',
-  'User Profiles' => '',
+    '%moduleName% - Set as default module' => '%moduleName% - تعيين كموديول افتراضي',
+    'Activated' => 'مفعل',
+    'Always activated' => 'مفعل دائماً',
+    'Close' => 'إغلاق',
+    'Deactivated' => 'معطل',
+    'Here you can choose whether or not a module should be automatically activated on a space or user profile. If the module should be activated, choose "always activated".' => 'يمكنك من هنا تحديد ما إذا كان ينبغي تفعيل الموديول تلقائياً عند إنشاء ساحة أو ملف شخصي للمستخدم. إذا كان من المفترض أي يكون الموديول فاعلاً، اختر "مفعل دائماً".',
+    'Save' => 'حفظ',
+    'Spaces' => 'الساحات',
+    'User Profiles' => 'ملفات المستخدمين',
 );

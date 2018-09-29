25a56ddf91b8550d182113797dc9f00edfb8560d
Ahmed Batis <mr.batis@gmail.com>
2018-09-29 18:33:32 +0400

    Update views_approval_approveUserDecline.php

--- protected/humhub/modules/admin/messages/ar/views_approval_approveUserDecline.php
+++ protected/humhub/modules/admin/messages/ar/views_approval_approveUserDecline.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'Cancel' => 'إلغاء',
-  'Decline & delete user: <strong>{displayName}</strong>' => 'رفض و حذف العضوية من : <strong>{displayName}</strong> ',
+    'Cancel' => 'إلغاء',
+    'Decline & delete user: <strong>{displayName}</strong>' => 'رفض وحذف المستخدم: <strong>{displayName}</strong>',
 );

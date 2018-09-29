845eff4f664849a33cc668bacd7d763224365fb3
Ahmed Batis <mr.batis@gmail.com>
2018-09-29 18:38:04 +0400

    Update views_approval_approveUserAccept.php

--- protected/humhub/modules/admin/messages/ar/views_approval_approveUserAccept.php
+++ protected/humhub/modules/admin/messages/ar/views_approval_approveUserAccept.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'Accept user: <strong>{displayName}</strong> ' => 'الموافقة على العضو: <strong>{displayName}</strong> ',
-  'Cancel' => 'إلغاء',
-  'Send & save' => 'حفظ و إرسال',
+    'Accept user: <strong>{displayName}</strong> ' => 'الموافقة على المستخدم: <strong>{displayName}</strong> ',
+    'Cancel' => 'إلغاء',
+    'Send & save' => 'حفظ وإرسال',
 );

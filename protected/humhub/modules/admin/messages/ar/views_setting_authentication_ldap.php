60c5866c07774ec5dd2fa42fc660b54158429a2b
Ahmed Batis <mr.batis@gmail.com>
2018-09-29 14:56:56 +0400

    Update views_setting_authentication_ldap.php

--- protected/humhub/modules/admin/messages/ar/views_setting_authentication_ldap.php
+++ protected/humhub/modules/admin/messages/ar/views_setting_authentication_ldap.php
@@ -17,17 +17,17 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'A TLS/SSL is strongly favored in production environments to prevent passwords from be transmitted in clear text.' => '',
+    'A TLS/SSL is strongly favored in production environments to prevent passwords from be transmitted in clear text.' => 'ينصح بشدة باستخدام طبقة حماية SSL أو TLS في بيئة مواقع الويب للحد من تسريب كلمات المرور كنص صريح.',
     'Defines the filter to apply, when login is attempted. %s replaces the username in the login action. Example: &quot;(sAMAccountName=%s)&quot; or &quot;(uid=%s)&quot;' => '',
     'LDAP Attribute for E-Mail Address. Default: &quotmail&quot;' => '',
     'LDAP Attribute for Username. Example: &quotuid&quot; or &quot;sAMAccountName&quot;' => '',
     'Limit access to users meeting this criteria. Example: &quot(objectClass=posixAccount)&quot; or &quot;(&(objectClass=person)(memberOf=CN=Workers,CN=Users,DC=myDomain,DC=com))&quot;' => '',
     'Specify your LDAP-backend used to fetch user accounts.' => '',
-    'Status: Error! (Message: {message})' => '',
-    'Status: OK! ({userCount} Users)' => '',
-    'Status: Warning! (No users found using the ldap user filter!)' => '',
+    'Status: Error! (Message: {message})' => 'الحالة: خطأ! (رسالة: {message})',
+    'Status: OK! ({userCount} Users)' => 'الحالة: تمام! (عدد المستخدمين {userCount})',
+    'Status: Warning! (No users found using the ldap user filter!)' => 'الحالة: تحذير! (لم يتم العثور على مستخدمين عبر فلتر LDAP)',
     'The default base DN used for searching for accounts.' => '',
-    'The default credentials password (used only with username above).' => '',
+    'The default credentials password (used only with username above).' => 'بيانات اعتماد كلمة المرور الافتراضية (للمستخدم المعين أعلاه)',
     'The default credentials username. Some servers require that this be in DN form. This must be given in DN form if the LDAP server requires a DN to bind and binding should be possible with simple usernames.' => '',
     'Save' => 'حفظ',
 ];

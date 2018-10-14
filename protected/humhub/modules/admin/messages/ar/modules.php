e15c24d2bcb72a64e314e193806be4d41d60a033
Ahmed Batis <mr.batis@gmail.com>
2018-10-14 16:34:37 +0400

    Update modules.php

--- protected/humhub/modules/admin/messages/ar/modules.php
+++ protected/humhub/modules/admin/messages/ar/modules.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'Ok' => 'تمام',
-  'The HumHub developers provide no support for third-party modules and neighter give any guarantee about the suitability, functionality or security of this module.' => '',
-  'Third-party disclaimer' => '',
+    'Ok' => 'تمام',
+    'The HumHub developers provide no support for third-party modules and neighter give any guarantee about the suitability, functionality or security of this module.' => 'لا يقدم مطورو هوم-هوب أي دعم لموديولات الطرف الثالث ولا يلتزمون بأي ضمانة تجاه التوافق، والأداء، والأمان لهذا الموديول.',
+    'Third-party disclaimer' => 'إخلاء مسؤولية تجاه الغير',
 );

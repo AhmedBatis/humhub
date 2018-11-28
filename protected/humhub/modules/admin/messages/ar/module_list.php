ae051cc1ea0fa8f88528e7f07d5b140034919b99
Ahmed Batis <mr.batis@gmail.com>
2018-11-28 22:26:08 +0400

    Update module_list.php

--- protected/humhub/modules/admin/messages/ar/module_list.php
+++ protected/humhub/modules/admin/messages/ar/module_list.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'Activated' => 'تم التفعيل',
-  'No modules installed yet. Install some to enhance the functionality!' => 'لا توجد موديولات بعد, قم بتحميل البعض لإضافة وظائف جديدة للموقع!',
-  'Version:' => 'نسخة: ',
+    'Activated' => 'تم تفعليه',
+    'No modules installed yet. Install some to enhance the functionality!' => 'ليس ثمة موديولات مثبته، قم بتثبيت بعض الموديولات لتعزيز وظائف الموقع.',
+    'Version:' => 'النسخة/الإصدار:',
 );
